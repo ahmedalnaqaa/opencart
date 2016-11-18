c0fbeb9f896b512df171a802d672913a1f0f1dbc
ahmedalnaqaa <ahmedalnaqaa@gmail.com>
2016-11-18 18:12:37 +0200

    Install vqmod

---
+++ catalog/model/extension/payment/cod_fee.php
@@ -0,0 +1,34 @@
+<?php
+class ModelExtensionPaymentCODFee extends Model {
+    public function getMethod($address, $total) {
+        $this->load->language('extension/payment/cod_fee');
+
+        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('cod_fee_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+
+        if ($this->config->get('cod_fee_total') > 0 && $this->config->get('cod_fee_total') > $total) {
+            $status = false;
+        } elseif (!$this->cart->hasShipping()) {
+            $status = false;
+        } elseif (!$this->config->get('cod_fee_geo_zone_id')) {
+            $status = true;
+        } elseif ($query->num_rows) {
+            $status = true;
+        } else {
+            $status = false;
+        }
+
+        $method_data = array();
+
+        if ($status) {
+            $method_data = array(
+                'code'       => 'cod_fee',
+                'title'      => $this->language->get('text_title'),
+                'terms'      => '',
+                'version'  => 209,
+                'sort_order' => $this->config->get('cod_fee_sort_order')
+            );
+        }
+
+        return $method_data;
+    }
+}
